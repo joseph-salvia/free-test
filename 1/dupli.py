bd999faf10e3fabce0033e5ed90c83c0dc3f0f76
joseph-salvia <uselesswbs@gmail.com>
2026-04-09 07:07:49 +0100

    Duplicate Sorter

---
+++ 1/dupli.py
@@ -0,0 +1,12 @@
+file_list = ["report.csv", 
+                "data.xlsx",
+                "summary.docx",
+                "report.csv",
+                "data.csv"]
+
+
+for file in file_list == (file):
+    print("Duplicate Data Detected")
+    break
+else:
+    pass
