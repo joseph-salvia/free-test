e0c5a6d415d755010fee34a51d0048db2d5f8797
joseph-salvia <uselesswbs@gmail.com>
2026-05-11 06:57:02 +0100

    An

---
+++ add.py
@@ -0,0 +1,16 @@
+#I want to display casing
+
+user_name = input("Hello, what may we address you as?\n\n")
+
+print("\nThank you for coming in " + user_name + ", Our Mocha is priced at $13.24\n")
+order_quantity = input("How many Mocha should we get you\n\n")
+
+mocha = 13.24
+
+print("\nAlright " + user_name + ", your order of " + str(order_quantity) + " Mocha will be ready soon\n")
+tip_amount = input("And how much tip will you like to give?\n\n")
+
+total = mocha * float(order_quantity) + float(tip_amount)
+
+print("Alright " + user_name + ", Your total will be $" + str(total) + ".\n" + "Kindly Proceed to the counter to pay and you'll get your order shortly" )
+
